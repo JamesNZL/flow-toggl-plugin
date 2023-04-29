8e3ca4ed8bde4be645f5f924d1113e7df293bde8
James <james@jamesnzl.xyz>
2023-04-29 12:46:56 +1200

    build: :technologist: update `dev` build script

--- build/dev.ps1
+++ build/dev.ps1
@@ -1,1 +1,16 @@
-dotnet publish ./src -o ./bin -c Debug -r win-x64 --no-self-contained
+$FlowProcess = Get-Process -Name "Flow.Launcher" -ErrorAction SilentlyContinue
+
+if ($FlowProcess) {
+    $FlowPath = $FlowProcess.Path
+    Stop-Process $FlowProcess
+
+    dotnet publish ./src -o ./bin -c Debug -r win-x64 --no-self-contained
+
+    Start-Process -FilePath $FlowPath
+}
+else
+{
+	echo "> ERROR: Flow.Launcher executable not currently running."
+	echo "> "
+	echo "> Please start Flow Launcher and try again."
+}

20ee5a3435c5471c6d3327733ef2998eaefe8b01
James <james@jamesnzl.xyz>
2023-05-21 15:05:43 +1200

    fix(reports): :bug: fix running time entry not appearing in report if spanning multiple days (#19)

--- src/TogglTrack.cs
+++ src/TogglTrack.cs
@@ -1637,7 +1637,7 @@ namespace Flow.Launcher.Plugin.TogglTrack
 
 			// Use cached time entry here to improve responsiveness
 			var runningTimeEntry = (await this._GetRunningTimeEntry())?.ToTimeEntry(me);
-			if ((runningTimeEntry is not null) && ((runningTimeEntry.StartDate.Date >= start.Date) && ((runningTimeEntry.StopDate ?? reportsNow).Date <= end.Date)))
+			if ((runningTimeEntry is not null) && ((runningTimeEntry.StartDate.Date >= start.Date) && ((runningTimeEntry.StartDate).Date <= end.Date)))
 			{
 				summary = summary?.InsertRunningTimeEntry(runningTimeEntry, groupingConfiguration.Grouping);
 			}
